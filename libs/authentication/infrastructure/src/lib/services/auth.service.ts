b4908fae6cd6bf719dff133e5e65801f5cf02f1a
Joan Serna <jsernaleiton@gmail.com>
2023-08-11 22:14:07 -0500

    Add all files on project

--- libs/authentication/infrastructure/src/lib/services/auth.service.ts
+++ libs/authentication/infrastructure/src/lib/services/auth.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { delay } from 'rxjs/operators';
-import { AuthDTO, AuthRepository, AuthResponse } from "@authentication/domain";
+import { AuthDTO, AuthRepository, AuthResponse } from '@authentication/domain';
 
 @Injectable({ providedIn: 'root' })
-export class AuthService extends AuthRepository{
-  login(authDTO:AuthDTO): Observable<AuthResponse> {
+export class AuthService extends AuthRepository {
+  login(authDTO: AuthDTO): Observable<AuthResponse> {
     // Simulate API call with a delay
-    console.log('request api winner')
+    console.log({ authDTO });
     return of({ token: 'mocked-token' }).pipe(delay(5000));
   }
 }
